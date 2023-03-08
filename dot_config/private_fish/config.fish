f10061ea279d3505a221f3fc4dfb7236d2d8f369
Johan Friis <johan.friis@lego.com>
2023-03-08 17:27:35 +0100

    update fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,13 @@
 if status is-interactive
-  # Commands to run in interactive sessions can go here
-  starship init fish | source
-  zoxide init --cmd j fish | source
+
+    if test -z (pgrep -n ssh-agent)
+        eval (ssh-agent -c) >/dev/null
+        set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+        set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    end
+
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+    zoxide init --cmd j fish | source
+
 end
