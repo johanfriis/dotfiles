de6a3213ce7fd16f43c275b19a3f6fffae5b9916
Johan Friis <johan@slowmail.it>
2023-03-09 20:12:42 +0100

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
+    # Commands to run in interactive sessions can go here
 
     if test -z (pgrep -n ssh-agent)
         eval (ssh-agent -c) >/dev/null
@@ -6,7 +7,6 @@ if status is-interactive
         set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
     end
 
-    # Commands to run in interactive sessions can go here
     starship init fish | source
     zoxide init --cmd j fish | source
 
