2e81f4516c3063b54df06537701673e383ec46e7
Johan Friis <johan@slowmail.it>
2023-03-09 20:51:03 +0100

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -419,15 +419,6 @@ require("lazy").setup({
         return math.floor(vim.o.columns * 0.75)
       end,
     },
-    init = function()
-      -- when noice is not enabled, install notify on VeryLazy
-      local Util = require("lazyvim.util")
-      if not Util.has("noice.nvim") then
-        Util.on_very_lazy(function()
-          vim.notify = require("notify")
-        end)
-      end
-    end,
   },
 
   -- better vim.ui
