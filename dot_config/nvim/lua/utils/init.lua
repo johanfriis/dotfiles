abb4fb3383b97c1c0f4eff8e0f8910911fc3b267
Johan Friis <johan@slowmail.it>
2023-03-10 16:30:00 +0100

    Update .config/nvim/lua/config/remap.lua Update .config/nvim/lua/utils/init.lua

--- dot_config/nvim/lua/utils/init.lua
+++ dot_config/nvim/lua/utils/init.lua
@@ -106,4 +106,36 @@ function M.opts(name)
 	return Plugin.values(plugin, "opts", false)
 end
 
+---@param silent boolean?
+---@param values? {[1]:any, [2]:any}
+function M.toggle(option, silent, values)
+	if values then
+		if vim.opt_local[option]:get() == values[1] then
+			vim.opt_local[option] = values[2]
+		else
+			vim.opt_local[option] = values[1]
+		end
+		return Util.info("Set " .. option .. " to " .. vim.opt_local[option]:get(), { title = "Option" })
+	end
+	vim.opt_local[option] = not vim.opt_local[option]:get()
+	if not silent then
+		if vim.opt_local[option]:get() then
+			Util.info("Enabled " .. option, { title = "Option" })
+		else
+			Util.warn("Disabled " .. option, { title = "Option" })
+		end
+	end
+end
+
+-- FIXME: create a togglable terminal
+-- Opens a floating terminal (interactive by default)
+---@param cmd? string[]|string
+---@param opts? LazyCmdOptions|{interactive?:boolean}
+function M.float_term(cmd, opts)
+	opts = vim.tbl_deep_extend("force", {
+		size = { width = 0.9, height = 0.9 },
+	}, opts or {})
+	require("lazy.util").float_term(cmd, opts)
+end
+
 return M
