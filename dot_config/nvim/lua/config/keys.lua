d37fa611422d56897f5d441e40aaabccf1909072
Johan Friis <johan@slowmail.it>
2023-03-10 16:18:36 +0100

    Update .config/nvim/lua/config/keys.lua Update .config/nvim/lua/utils/init.lua

--- dot_config/nvim/lua/config/keys.lua
+++ dot_config/nvim/lua/config/keys.lua
@@ -1,42 +1,46 @@
-local Utils = require('utils')
+local Utils = require("utils")
+
+-- better up/down
+Utils.map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+Utils.map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
 return {
 
-  whichKey = function ()
-    local keymaps = {
-      mode = { "n", "v" },
-      ["g"] = { name = "+goto" },
-      ["gz"] = { name = "+surround" },
-      -- ["]"] = { name = "+next" },
-      -- ["["] = { name = "+prev" },
-      -- ["<leader><tab>"] = { name = "+tabs" },
-      ["<leader>b"] = { name = "+buffer" },
-      -- ["<leader>c"] = { name = "+code" },
-      -- ["<leader>f"] = { name = "+file/find" },
-      ["<leader>g"] = { name = "+git" },
-      -- ["<leader>gh"] = { name = "+hunks" },
-      ["<leader>q"] = { name = "+quit/session" },
-      -- ["<leader>s"] = { name = "+search" },
-      -- ["<leader>u"] = { name = "+ui" },
-      -- ["<leader>w"] = { name = "+windows" },
-      -- ["<leader>x"] = { name = "+diagnostics/quickfix" },
-    }
-    if Utils.has("noice.nvim") then
-      keymaps["<leader>sn"] = { name = "+noice" }
-    end
-    return keymaps
-  end,
+	whichKey = function()
+		local keymaps = {
+			mode = { "n", "v" },
+			["g"] = { name = "+goto" },
+			["gz"] = { name = "+surround" },
+			-- ["]"] = { name = "+next" },
+			-- ["["] = { name = "+prev" },
+			-- ["<leader><tab>"] = { name = "+tabs" },
+			["<leader>b"] = { name = "+buffer" },
+			-- ["<leader>c"] = { name = "+code" },
+			-- ["<leader>f"] = { name = "+file/find" },
+			["<leader>g"] = { name = "+git" },
+			-- ["<leader>gh"] = { name = "+hunks" },
+			["<leader>q"] = { name = "+quit/session" },
+			-- ["<leader>s"] = { name = "+search" },
+			-- ["<leader>u"] = { name = "+ui" },
+			-- ["<leader>w"] = { name = "+windows" },
+			-- ["<leader>x"] = { name = "+diagnostics/quickfix" },
+		}
+		if Utils.has("noice.nvim") then
+			keymaps["<leader>sn"] = { name = "+noice" }
+		end
+		return keymaps
+	end,
 
-  mason = {
-    { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } 
-  },
+	mason = {
+		{ "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" },
+	},
 
-  gitsigns = function (buffer)
-    local gs = package.loaded.gitsigns
+	gitsigns = function(buffer)
+		local gs = package.loaded.gitsigns
 
-    local function map(mode, l, r, desc)
-      vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
-    end
+		local function map(mode, l, r, desc)
+			vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+		end
 
     -- stylua: ignore start
     map("n", "]h", gs.next_hunk, "Next Hunk")
@@ -51,189 +55,291 @@ return {
     map("n", "<leader>ghd", gs.diffthis, "Diff This")
     map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
     map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
-  end,
+	end,
 
-  leap = {
-    { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
-    { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
-    { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
-  },
+	leap = {
+		{ "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+		{ "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+		{ "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+	},
 
-  luasnip = {
-    {
-      "<tab>",
-      function()
-        return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-      end,
-      expr = true, silent = true, mode = "i",
-    },
-    { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
-    { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
-  },
+	luasnip = {
+		{
+			"<tab>",
+			function()
+				return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+			end,
+			expr = true,
+			silent = true,
+			mode = "i",
+		},
+		{
+			"<tab>",
+			function()
+				require("luasnip").jump(1)
+			end,
+			mode = "s",
+		},
+		{
+			"<s-tab>",
+			function()
+				require("luasnip").jump(-1)
+			end,
+			mode = { "i", "s" },
+		},
+	},
 
-  treesitter = {
-    { "<c-space>", desc = "Increment selection" },
-    { "<bs>", desc = "Decrement selection", mode = "x" },
-  },
+	treesitter = {
+		{ "<c-space>", desc = "Increment selection" },
+		{ "<bs>", desc = "Decrement selection", mode = "x" },
+	},
 
-  telescope = {
-      { "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
-      { "<leader>/", Utils.telescope("live_grep"), desc = "Find in Files (Grep)" },
-      { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-      { "<leader><space>", Utils.telescope("files"), desc = "Find Files (root dir)" },
-      -- find
-      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-      { "<leader>ff", Utils.telescope("files"), desc = "Find Files (root dir)" },
-      { "<leader>fF", Utils.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
-      { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
-      -- git
-      { "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "commits" },
-      { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "status" },
-      -- search
-      { "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
-      { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
-      { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-      { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
-      { "<leader>sd", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
-      { "<leader>sg", Utils.telescope("live_grep"), desc = "Grep (root dir)" },
-      { "<leader>sG", Utils.telescope("live_grep", { cwd = false }), desc = "Grep (cwd)" },
-      { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
-      { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
-      { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
-      { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
-      { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
-      { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
-      { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
-      { "<leader>sw", Utils.telescope("grep_string"), desc = "Word (root dir)" },
-      { "<leader>sW", Utils.telescope("grep_string", { cwd = false }), desc = "Word (cwd)" },
-      { "<leader>uC", Utils.telescope("colorscheme", { enable_preview = true }), desc = "Colorscheme with preview" },
-      {
-        "<leader>ss",
-        Utils.telescope("lsp_document_symbols", {
-          symbols = {
-            "Class",
-            "Function",
-            "Method",
-            "Constructor",
-            "Interface",
-            "Module",
-            "Struct",
-            "Trait",
-            "Field",
-            "Property",
-          },
-        }),
-        desc = "Goto Symbol",
-      },
-      {
-        "<leader>sS",
-        Utils.telescope("lsp_workspace_symbols", {
-          symbols = {
-            "Class",
-            "Function",
-            "Method",
-            "Constructor",
-            "Interface",
-            "Module",
-            "Struct",
-            "Trait",
-            "Field",
-            "Property",
-          },
-        }),
-        desc = "Goto Symbol (Workspace)",
-      },
-  },
+	telescope = {
+		{ "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
+		{ "<leader>/", Utils.telescope("live_grep"), desc = "Find in Files (Grep)" },
+		{ "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+		{ "<leader><space>", Utils.telescope("files"), desc = "Find Files (root dir)" },
+		-- find
+		{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+		{ "<leader>ff", Utils.telescope("files"), desc = "Find Files (root dir)" },
+		{ "<leader>fF", Utils.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
+		{ "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+		-- git
+		{ "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "commits" },
+		{ "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "status" },
+		-- search
+		{ "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
+		{ "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
+		{ "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+		{ "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
+		{ "<leader>sd", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
+		{ "<leader>sg", Utils.telescope("live_grep"), desc = "Grep (root dir)" },
+		{ "<leader>sG", Utils.telescope("live_grep", { cwd = false }), desc = "Grep (cwd)" },
+		{ "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
+		{ "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
+		{ "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
+		{ "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
+		{ "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
+		{ "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
+		{ "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
+		{ "<leader>sw", Utils.telescope("grep_string"), desc = "Word (root dir)" },
+		{ "<leader>sW", Utils.telescope("grep_string", { cwd = false }), desc = "Word (cwd)" },
+		{ "<leader>uC", Utils.telescope("colorscheme", { enable_preview = true }), desc = "Colorscheme with preview" },
+		{
+			"<leader>ss",
+			Utils.telescope("lsp_document_symbols", {
+				symbols = {
+					"Class",
+					"Function",
+					"Method",
+					"Constructor",
+					"Interface",
+					"Module",
+					"Struct",
+					"Trait",
+					"Field",
+					"Property",
+				},
+			}),
+			desc = "Goto Symbol",
+		},
+		{
+			"<leader>sS",
+			Utils.telescope("lsp_workspace_symbols", {
+				symbols = {
+					"Class",
+					"Function",
+					"Method",
+					"Constructor",
+					"Interface",
+					"Module",
+					"Struct",
+					"Trait",
+					"Field",
+					"Property",
+				},
+			}),
+			desc = "Goto Symbol (Workspace)",
+		},
+	},
 
-  neotree = {
-    {
-      "<leader>fe",
-      function()
-        require("neo-tree.command").execute({ toggle = true, dir = require('utils').get_root() })
-      end,
-      desc = "Explorer NeoTree (root dir)",
-    },
-    {
-      "<leader>fE",
-      function()
-        require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
-      end,
-      desc = "Explorer NeoTree (cwd)",
-    },
-    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
-    { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
-  },
+	neotree = {
+		{
+			"<leader>fe",
+			function()
+				require("neo-tree.command").execute({ toggle = true, dir = require("utils").get_root() })
+			end,
+			desc = "Explorer NeoTree (root dir)",
+		},
+		{
+			"<leader>fE",
+			function()
+				require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+			end,
+			desc = "Explorer NeoTree (cwd)",
+		},
+		{ "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
+		{ "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+	},
 
-  noice = {
-    -- { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
-    { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
-    { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
-    { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
-    { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
-    { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
-  },
+	noice = {
+		-- { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+		{
+			"<leader>snl",
+			function()
+				require("noice").cmd("last")
+			end,
+			desc = "Noice Last Message",
+		},
+		{
+			"<leader>snh",
+			function()
+				require("noice").cmd("history")
+			end,
+			desc = "Noice History",
+		},
+		{
+			"<leader>sna",
+			function()
+				require("noice").cmd("all")
+			end,
+			desc = "Noice All",
+		},
+		{
+			"<c-f>",
+			function()
+				if not require("noice.lsp").scroll(4) then
+					return "<c-f>"
+				end
+			end,
+			silent = true,
+			expr = true,
+			desc = "Scroll forward",
+			mode = { "i", "n", "s" },
+		},
+		{
+			"<c-b>",
+			function()
+				if not require("noice.lsp").scroll(-4) then
+					return "<c-b>"
+				end
+			end,
+			silent = true,
+			expr = true,
+			desc = "Scroll backward",
+			mode = { "i", "n", "s" },
+		},
+	},
 
-  persistence = {
-    { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
-    { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-    { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
-  },
+	persistence = {
+		{
+			"<leader>qs",
+			function()
+				require("persistence").load()
+			end,
+			desc = "Restore Session",
+		},
+		{
+			"<leader>ql",
+			function()
+				require("persistence").load({ last = true })
+			end,
+			desc = "Restore Last Session",
+		},
+		{
+			"<leader>qd",
+			function()
+				require("persistence").stop()
+			end,
+			desc = "Don't Save Current Session",
+		},
+	},
 
-  -- search/replace in multiple files
-  spectre = {
-    { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
-  },
+	-- search/replace in multiple files
+	spectre = {
+		{
+			"<leader>sr",
+			function()
+				require("spectre").open()
+			end,
+			desc = "Replace in files (Spectre)",
+		},
+	},
 
-  -- use gz mappings instead of s to prevent conflict with leap
-  surround = {
-    add = "gza",            -- Add surrounding in Normal and Visual modes
-    delete = "gzd",         -- Delete surrounding
-    find = "gzf",           -- Find surrounding (to the right)
-    find_left = "gzF",      -- Find surrounding (to the left)
-    highlight = "gzh",      -- Highlight surrounding
-    replace = "gzr",        -- Replace surrounding
-    update_n_lines = "gzn", -- Update `n_lines`
-  },
+	-- use gz mappings instead of s to prevent conflict with leap
+	surround = {
+		add = "gza", -- Add surrounding in Normal and Visual modes
+		delete = "gzd", -- Delete surrounding
+		find = "gzf", -- Find surrounding (to the right)
+		find_left = "gzF", -- Find surrounding (to the left)
+		highlight = "gzh", -- Highlight surrounding
+		replace = "gzr", -- Replace surrounding
+		update_n_lines = "gzn", -- Update `n_lines`
+	},
 
-  todo_comments = {
-    { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
-    { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
-    -- { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
-    -- { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
-    -- { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
-  },
+	todo_comments = {
+		{
+			"]t",
+			function()
+				require("todo-comments").jump_next()
+			end,
+			desc = "Next todo comment",
+		},
+		{
+			"[t",
+			function()
+				require("todo-comments").jump_prev()
+			end,
+			desc = "Previous todo comment",
+		},
+		-- { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
+		-- { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+		-- { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+	},
 
-  trouble = {
-    { "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
-    { "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
-    { "<leader>xL", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
-    { "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
-    {
-      "[q",
-      function()
-        if require("trouble").is_open() then
-          require("trouble").previous({ skip_groups = true, jump = true })
-        else
-          vim.cmd.cprev()
-        end
-      end,
-      desc = "Previous trouble/quickfix item",
-    },
-    {
-      "]q",
-      function()
-        if require("trouble").is_open() then
-          require("trouble").next({ skip_groups = true, jump = true })
-        else
-          vim.cmd.cnext()
-        end
-      end,
-      desc = "Next trouble/quickfix item",
-    },
-  },
+	trouble = {
+		{ "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
+		{ "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
+		{ "<leader>xL", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
+		{ "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
+		{
+			"[q",
+			function()
+				if require("trouble").is_open() then
+					require("trouble").previous({ skip_groups = true, jump = true })
+				else
+					vim.cmd.cprev()
+				end
+			end,
+			desc = "Previous trouble/quickfix item",
+		},
+		{
+			"]q",
+			function()
+				if require("trouble").is_open() then
+					require("trouble").next({ skip_groups = true, jump = true })
+				else
+					vim.cmd.cnext()
+				end
+			end,
+			desc = "Next trouble/quickfix item",
+		},
+	},
 
-  bufremove = {
-    { "<leader>bd", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
-    { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
-  }
+	bufremove = {
+		{
+			"<leader>bd",
+			function()
+				require("mini.bufremove").delete(0, false)
+			end,
+			desc = "Delete Buffer",
+		},
+		{
+			"<leader>bD",
+			function()
+				require("mini.bufremove").delete(0, true)
+			end,
+			desc = "Delete Buffer (Force)",
+		},
+	},
 }
