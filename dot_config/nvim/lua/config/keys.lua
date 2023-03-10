879a5ddde26e4a0b12e622fa5a607de5c2306b19
Johan Friis <johan@slowmail.it>
2023-03-10 16:21:47 +0100

    Update .config/nvim/lua/config/keys.lua Update .config/nvim/lua/config/options.lua Add .config/nvim/lua/config/remap.lua

--- dot_config/nvim/lua/config/keys.lua
+++ dot_config/nvim/lua/config/keys.lua
@@ -1,9 +1,5 @@
 local Utils = require("utils")
 
--- better up/down
-Utils.map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-Utils.map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-
 return {
 
 	whichKey = function()
--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,13 +1,15 @@
 vim.g.mapleader = " "
 
+require("config.remap")
+
 vim.opt.clipboard = "unnamedplus" -- Sync with system clipboard
-vim.opt.expandtab = true 					-- Use spaces instead of tabs
-vim.opt.number = true 						-- Print line number
-vim.opt.relativenumber = true 		-- Relative line numbers
-vim.opt.scrolloff = 4 						-- Lines of context
-vim.opt.shiftwidth = 2 						-- Size of an indent
-vim.opt.signcolumn = "yes"        -- Always show the signcolumn, otherwise it would shift the text each time
-vim.opt.smartindent = true 				-- Insert indents automatically
-vim.opt.tabstop = 2 							-- Number of spaces tabs count for
-vim.opt.termguicolors = true      -- Enable 24bit colour
-vim.opt.wrap = false 							-- Disable line wrap
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.number = true -- Print line number
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.scrolloff = 4 -- Lines of context
+vim.opt.shiftwidth = 2 -- Size of an indent
+vim.opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+vim.opt.smartindent = true -- Insert indents automatically
+vim.opt.tabstop = 2 -- Number of spaces tabs count for
+vim.opt.termguicolors = true -- Enable 24bit colour
+vim.opt.wrap = false -- Disable line wrap
