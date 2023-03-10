a66f087aa7269689404102f53c77e10f1057ec59
Johan Friis <johan@slowmail.it>
2023-03-10 19:23:38 +0100

    Update .config/nvim/lua/plugins/zen.lua

--- dot_config/nvim/lua/plugins/zen.lua
+++ dot_config/nvim/lua/plugins/zen.lua
@@ -3,6 +3,15 @@ return {
   opts = {
     window = {
       backdrop = 1,
+      options = {
+        signcolumn = "no",
+        number = false,
+        relativenumber = false,
+        cursorline = false,
+        cursorcolumn = false,
+        foldcolumn = false,
+        list = false,
+      }
     },
   },
   dependencies = {
