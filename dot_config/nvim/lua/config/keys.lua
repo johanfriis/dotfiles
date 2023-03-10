5f54274f26afd01d41eacce8d8c0307f50f85614
Johan Friis <johan@slowmail.it>
2023-03-10 15:34:06 +0100

    Update .config/nvim/lua/config/keys.lua Add .config/nvim/lua/plugins/trouble.lua

--- dot_config/nvim/lua/config/keys.lua
+++ dot_config/nvim/lua/config/keys.lua
@@ -199,6 +199,35 @@ return {
     -- { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
   },
 
+  trouble = {
+    { "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
+    { "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
+    { "<leader>xL", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
+    { "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
+    {
+      "[q",
+      function()
+        if require("trouble").is_open() then
+          require("trouble").previous({ skip_groups = true, jump = true })
+        else
+          vim.cmd.cprev()
+        end
+      end,
+      desc = "Previous trouble/quickfix item",
+    },
+    {
+      "]q",
+      function()
+        if require("trouble").is_open() then
+          require("trouble").next({ skip_groups = true, jump = true })
+        else
+          vim.cmd.cnext()
+        end
+      end,
+      desc = "Next trouble/quickfix item",
+    },
+  },
+
   bufremove = {
     { "<leader>bd", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
     { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
