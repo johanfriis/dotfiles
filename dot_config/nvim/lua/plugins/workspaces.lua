aef5fcd9a1b577ce06bd19c9eb775bdf0a78a28e
Johan Friis <johan@slowmail.it>
2023-03-10 19:11:33 +0100

    Update .config/nvim/lua/plugins/workspaces.lua

--- dot_config/nvim/lua/plugins/workspaces.lua
+++ dot_config/nvim/lua/plugins/workspaces.lua
@@ -1,6 +1,9 @@
 return {
   {
     "natecraddock/workspaces.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
     config = function()
       require('telescope').load_extensions('workspaces')
     end
