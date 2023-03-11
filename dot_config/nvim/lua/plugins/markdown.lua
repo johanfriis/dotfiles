4e5923a2729c5c20a8f5d184cb31411b4506c126
Johan Friis <johan@slowmail.it>
2023-03-11 12:39:24 +0100

    Update .config/nvim/lua/plugins/markdown.lua Change attributes of .config/nvim/lua/plugins/fish.lua Change attributes of .config/nvim/lua/plugins/json.lua Change attributes of .config/nvim/lua/plugins/lua.lua Change attributes of .config/nvim/lua/plugins/shell.lua Change attributes of .config/nvim/lua/plugins/typescript.lua

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -3,8 +3,8 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
-      table.insert(opts.sources, nls.builtins.diagnostics.markdownlint)
-      table.insert(opts.sources, nls.builtins.formatting.cbfmt)
+      -- table.insert(opts.sources, nls.builtins.diagnostics.markdownlint)
+      -- table.insert(opts.sources, nls.builtins.formatting.cbfmt)
     end,
   },
   -- {
