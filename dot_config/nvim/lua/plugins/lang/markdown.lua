f33b3dfd3ab303e55b6f3e9b9f905df7d79f69d5
Johan Friis <johan@slowmail.it>
2023-03-11 12:24:56 +0100

    Add .config/nvim/lua/plugins/lang/markdown.lua Remove .config/nvim/lua/plugins/markdown.lua Remove .config/nvim/lua/plugins/telekasten.lua Remove .config/nvim/lua/plugins/zk.lua

---
+++ dot_config/nvim/lua/plugins/lang/markdown.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      table.insert(opts.sources, nls.builtins.diagnostics.markdownlint)
+      table.insert(opts.sources, nls.builtins.formatting.cbfmt)
+    end,
+  },
+  -- {
+  --   "ixru/nvim-markdown",
+  --   branch = "master",
+  --   dependencies = {
+  --     {
+  --       "godlygeek/tabular"
+  --     }
+  --   },
+  --   config = function()
+  --   end,
+  -- },
+
+  {
+    "jakewvincent/mkdnflow.nvim",
+    -- rocks = 'luautf8', -- Ensures optional luautf8 dependency is installed
+    config = function()
+      require('mkdnflow').setup()
+    end
+  },
+
+
+  -- {
+  --   "mickael-menu/zk-nvim",
+  --   config = function()
+  --     require("zk").setup()
+  --     require("telescope").load_extension("zk")
+  --   end
+  -- },
+  --
+  -- {
+  --   "renerocksai/telekasten.nvim",
+  --   opts = {
+  --     home = vim.fn.expand("~/Code/vault/")
+  --   },
+  --   dependencies = {
+  --     {
+  --       "renerocksai/calendar-vim",
+  --     }
+  --   }
+  -- },
+
+}
--- dot_config/nvim/lua/plugins/zk.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "mickael-menu/zk-nvim",
-  config = function()
-    require("zk").setup()
-    require("telescope").load_extension("zk")
-  end
-}
