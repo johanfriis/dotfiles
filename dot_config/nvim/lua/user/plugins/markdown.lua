2fd1462d8d1bca8500886b378ac62ecf90f3fcf1
Johan Friis <johan@slowmail.it>
2023-03-15 14:52:48 +0100

    Update .config/nvim/lua/user/plugins/markdown.lua Remove .config/nvim/lua/user/plugins/zk.lua

--- dot_config/nvim/lua/user/plugins/markdown.lua
+++ dot_config/nvim/lua/user/plugins/markdown.lua
@@ -1,49 +1,70 @@
-local Utils = require('user.utils')
-
-local hexa = '0123456789abcdef'
+-- local Utils = require('user.utils')
+--
+-- local hexa = '0123456789abcdef'
 
 return {
+  -- {
+  --   'jakewvincent/mkdnflow.nvim',
+  --   opts = {
+  --     mappings = {
+  --       -- MkdnTab = { { 'n', 'v' }, '<Tab>' },
+  --       -- MkdnSTab = { { 'n', 'v' }, '<S-Tab>' },
+  --       MkdnToggleToDo = { { 'n', 'v' }, '<C-Enter>' },
+  --       MkdnFoldSection = false,
+  --       MkdnUnfoldSection = false,
+  --     },
+  --     modules = {
+  --       bib = false,
+  --     },
+  --     perspective = {
+  --       priority = 'root',
+  --       root_tell = '.git',
+  --       fallback = 'current',
+  --     },
+  --     links = {
+  --       style = 'wiki',
+  --       conceal = true,
+  --       implicit_extension = 'md',
+  --       transform_explicit = function(_)
+  --         return Utils.random_string(6, hexa)
+  --       end,
+  --       transform_implicit = function(text)
+  --         print(text)
+  --         return text
+  --       end,
+  --     },
+  --   },
+  -- },
+  -- {
+  --   'ellisonleao/glow.nvim',
+  --   cmd = 'Glow',
+  --   opts = {
+  --     width_ratio = 0.9,
+  --     height_ratio = 0.9,
+  --     pager = true,
+  --     border = 'single',
+  --   },
+  --   config = true,
+  -- },
   {
-    'jakewvincent/mkdnflow.nvim',
-    opts = {
-      mappings = {
-        -- MkdnTab = { { 'n', 'v' }, '<Tab>' },
-        -- MkdnSTab = { { 'n', 'v' }, '<S-Tab>' },
-        MkdnToggleToDo = { { 'n', 'v' }, '<C-Enter>' },
-        MkdnFoldSection = false,
-        MkdnUnfoldSection = false,
-      },
-      modules = {
-        bib = false,
-      },
-      perspective = {
-        priority = 'root',
-        root_tell = '.git',
-        fallback = 'current',
-      },
-      links = {
-        style = 'wiki',
-        conceal = true,
-        implicit_extension = 'md',
-        transform_explicit = function(_)
-          return Utils.random_string(6, hexa)
-        end,
-        transform_implicit = function(text)
-          print(text)
-          return text
-        end,
-      },
-    },
-  },
-  {
-    'ellisonleao/glow.nvim',
-    cmd = 'Glow',
-    opts = {
-      width_ratio = 0.9,
-      height_ratio = 0.9,
-      pager = true,
-      border = 'single',
-    },
-    config = true,
+    'mickael-menu/zk-nvim',
+    config = function()
+      require('zk').setup({
+        picker = 'telescope',
+        lsp = {
+          -- `config` is passed to `vim.lsp.start_client(config)`
+          config = {
+            cmd = { 'zk', 'lsp' },
+            name = 'zk',
+            -- on_attach = ...
+            -- etc, see `:h vim.lsp.start_client()`
+          },
+          auto_attach = {
+            enabled = false,
+            filetypes = { 'markdown' },
+          },
+        },
+      })
+    end,
   },
 }
