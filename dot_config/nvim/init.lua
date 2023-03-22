bd0f9fbc1d071388d28c2a6ed4d31903e9df4816
Johan Friis <johan@slowmail.it>
2023-03-22 10:39:09 +0100

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,5 +1,7 @@
-require('user.config.options')
-
-require('user.lazy')
-
-require('user.config.keymap')
+if vim.g.vscode then
+  require('user.config.options')
+else
+  require('user.config.options')
+  require('user.lazy')
+  require('user.config.keymap')
+end
