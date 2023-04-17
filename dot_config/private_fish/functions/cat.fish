1964d70157f3e2d3d163471c22224b44656f46be
Johan Friis <johan@slowmail.it>
2023-04-17 14:27:34 +0200

    Update .config/fish/functions/cat.fish

--- dot_config/private_fish/functions/cat.fish
+++ dot_config/private_fish/functions/cat.fish
@@ -1,6 +1,3 @@
 function cat --wraps=bat --description 'alias cat bat'
-  bat --style="grid,changes" --tabs 2 --theme zenburn $argv
-  # To take advantage of dark mode, use the following
-  # alias cat="bat --theme=\$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo default || echo GitHub)"
+    bat
 end
-
