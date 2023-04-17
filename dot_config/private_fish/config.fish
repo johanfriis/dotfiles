f5422d369d659b50a984af296bcfc9190e9b6f40
Johan Friis <johan@slowmail.it>
2023-04-17 11:20:09 +0200

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,8 +3,8 @@ if status is-interactive
 
     if test -z (pgrep -n ssh-agent)
         eval (ssh-agent -c) >/dev/null
-        set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-        set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+        set -gx SSH_AGENT_PID $SSH_AGENT_PID
+        set -gx SSH_AUTH_SOCK $SSH_AUTH_SOCK
     end
 
     starship init fish | source
