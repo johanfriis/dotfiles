54354e2b2e585adc47d6f2af0615a5b1182c0aa6
Johan Friis <johan@slowmail.it>
2023-04-25 00:12:02 +0200

    Add .config/emacs/#configuration.org# Add .config/emacs/configuration.el Add .config/emacs/configuration.org Add .config/emacs/custom.el Add .config/emacs/init.bak.el Update .config/emacs/init.el

---
+++ dot_config/private_emacs/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4d16802de4686030ed8f30b5a844713d68edec9cc07322bef54493d15e68d8cd" "bb6d3df2670c74f4ad513512feea89cdbe3040c1d0ba00cf641d63679ee3d0fc" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
