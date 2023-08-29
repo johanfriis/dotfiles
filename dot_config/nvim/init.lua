a4b70ec81b8b9a9a0922f7848c394b9d1524832a
Johan Friis <johan@slowmail.it>
2023-08-29 09:55:30 +0200

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -168,7 +168,7 @@ require("lazy").setup({
 		opts = {
 			options = {
 				icons_enabled = false,
-				theme = "onedark",
+				theme = "solarized_light",
 				component_separators = "|",
 				section_separators = "",
 			},
