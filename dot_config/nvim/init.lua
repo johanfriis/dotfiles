5e12c80ba9c61e18ffedf9271d03d27056e90f83
Johan Friis <johan@slowmail.it>
2023-08-29 10:25:29 +0200

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -181,7 +181,7 @@ require("lazy").setup({
 		-- Enable `lukas-reineke/indent-blankline.nvim`
 		-- See `:help indent_blankline.txt`
 		opts = {
-			char = "?",
+			char = "┊",
 			show_trailing_blankline_indent = false,
 		},
 	},
