6728088d7069803ad0dbea272599dcc8665a6b09
Johan Friis <johan@slowmail.it>
2023-09-14 12:24:33 +0200

    Add .config/wezterm/wezterm.lua

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,42 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder() then
+    config = wezterm.config_builder()
+end
+
+-- // COLOR SCHEME
+
+local function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+local function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'rose-pine-moon'
+  else
+    return 'rose-pine-dawn'
+  end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
+
+-- // FONT
+
+config.font = wezterm.font("CaskaydiaCove Nerd Font", {
+    weight = "Regular",
+    stretch = "Normal",
+    style = "Normal",
+    italic = true,
+})
+config.font_size = 13
+
+-- // UI
+config.enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
