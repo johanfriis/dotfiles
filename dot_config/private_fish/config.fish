bcd8e4a2bf548bd508095a4bcff8bf0f09ca1a54
Johan Friis <johan@slowmail.it>
2023-10-20 12:43:52 +0200

    Update .config/fish/config.fish Update .config/fish/fish_plugins Update .config/fish/fish_variables

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,14 +1,51 @@
-if status is-interactive
-  # Commands to run in interactive sessions can go here
+# #
+# # Init file for fish
+# #
 
-  if test -z (pgrep -n ssh-agent)
-    eval (ssh-agent -c) >/dev/null
-    set -gx SSH_AGENT_PID $SSH_AGENT_PID
-    set -gx SSH_AUTH_SOCK $SSH_AUTH_SOCK
-  end
+if not status is-interactive
+    exit
+end
 
+# setup some local directories
+set --local DEV_DIR "$HOME/dev"
+set --local TOOLS_DIR "$DEV_DIR/tools"
+set --local BIN_DIR "$DEV_DIR/bin"
+
+# prepend homebrew to PATH
+set --global --prepend --path fish_user_paths /opt/homebrew/bin
+
+# https://github.com/asdf-vm/asdf
+if command -q brew
+	set --global --export ASDF_DATA_DIR "$TOOLS_DIR/asdf"
+	source (brew --prefix asdf)/libexec/asdf.fish
+end
+
+# prepend home-relative dirs to PATH. These have priority over system dirs
+set --global --prepend --path fish_user_paths \
+	"$BIN_DIR" \
+	"$HOME/.config/tmux/plugins/t-smart-tmux-session-manager/bin" \
+	"$HOME/.config/tmux/plugins/t-smart-tmux-session-manager/bin" \
+	"$(brew --prefix coreutils)/libexec/gnubin" \
+	"$(brew --prefix)/sbin" \
+	"$(brew --prefix)/bin"
+
+# initialize variables
+set --global --export EDITOR nvim
+set --global --export FZF_TMUX_OPTS -p 55%,60%
+
+# https://starship.rs
+if command -q starship
   starship init fish | source
-  zoxide init --cmd j fish | source
+end
 
-  fish_add_path $HOME/.config/tmux/plugins/t-smart-tmux-session-manager/bin
+# https://github.com/ajeetdsouza/zoxide
+if command -q zoxide
+  zoxide init --cmd j fish | source
+end
+
+# start ssh-agent
+if test -z (pgrep ssh-agent | string collect)
+	eval (ssh-agent -c) >/dev/null
+	set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+	set -Ux SSH_AGENT_PID $SSH_AGENT_PID
 end
